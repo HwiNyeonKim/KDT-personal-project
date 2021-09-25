9e2b9506b23f4789613fdf0a26fcd78d6795ee79
HwiNyeon <z2b23173us@gmail.com>
2021-09-25 22:26:00 +0900

    [Refactor] - Change return type of detail page api

--- src/main/java/com/example/cccoffeebackendfromatoz/controller/api/ProductRestController.java
+++ src/main/java/com/example/cccoffeebackendfromatoz/controller/api/ProductRestController.java
@@ -5,6 +5,7 @@ import com.example.cccoffeebackendfromatoz.model.product.Category;
 import com.example.cccoffeebackendfromatoz.model.product.Product;
 import com.example.cccoffeebackendfromatoz.product.service.ProductService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.sql.Timestamp;
 import java.util.*;
@@ -22,10 +23,9 @@ public class ProductRestController {
 	public List<Product> productList(@RequestParam Optional<Category> category,
 	                                 @RequestParam Optional<Long> priceMin,
 	                                 @RequestParam Optional<Long> priceMax,
-	                                 @RequestParam(defaultValue = "1970-01-01") String createdDateFrom,
-	                                 @RequestParam(defaultValue = "9999-12-31") String createdDateTo,
 	                                 @RequestParam Optional<String> productName,
-	                                 @RequestParam Optional<UUID> productId) {
+	                                 @RequestParam(defaultValue = "1970-01-01") String createdDateFrom,
+	                                 @RequestParam(defaultValue = "9999-12-31") String createdDateTo) {
 		// search with created date range condition
 		Timestamp createdDateTimeFrom = Timestamp.valueOf(createdDateFrom + " 00:00:00");
 		Timestamp createdDateTimeTo = Timestamp.valueOf(createdDateTo + " 23:59:59");
@@ -34,11 +34,6 @@ public class ProductRestController {
 		// search with product name
 		if (productName.isPresent()) {
 			products = products.stream().filter(product -> product.getProductName().equals(productName.get())).toList();
-		}
-
-		// search with product ID
-		if (productId.isPresent()) {
-			products = products.stream().filter(product -> product.getProductId().equals(productId.get())).toList();
 		}
 
 		// search with category condition
@@ -59,29 +54,13 @@ public class ProductRestController {
 
 	// Get a certain product information by product ID
 	@GetMapping("/api/v1/products/{productId}")
-	public Map<String, String> getCertainProduct(@PathVariable("productId") UUID productId) {
-		Map<String, String> result = new HashMap<>();
-
-		List<Product> productData = service.getProductById(productId);
-		if (productData.isEmpty()) {
-			return result;
-		}
-
-		Product product = productData.get(0);
-		result.put("productId", product.getProductId().toString());
-		result.put("productName", product.getProductName());
-		result.put("category", product.getCategory().toString());
-		result.put("price", String.valueOf(product.getPrice()));
-		result.put("description", product.getDescription());
-		result.put("createdAt", product.getCreatedAt().toString());
-		result.put("lastModifiedAt", product.getLastModifiedAt().toString());
-
-		return result;
+	public List<Product> getCertainProduct(@PathVariable("productId") UUID productId) {
+		return service.getProductById(productId); // empty if no such product exist.
 	}
 
 	// Create a product
 	@PostMapping("/api/v1/products")
-	public Product createProduct(@ModelAttribute("createProductRequest") CreateProductRequest createProductRequest) {
+	public Product createProduct(@RequestBody CreateProductRequest createProductRequest) {
 		return service.createProduct(createProductRequest.productName(),
 				createProductRequest.category(),
 				createProductRequest.price(),
